3164f3a2537cb842983c3b006be954755adf04c6
ghada <ghada.maalej@esprit.tn>
2024-10-11 16:07:02 +0100

    ajout fichier

--- Main.java
+++ Main.java
@@ -1,18 +1,66 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-       Animal lion = new Animal();
-        Zoo zoo = new Zoo();
-        zoo.nbrCages=25;
-        Animal lion2 = new Animal("lion","aa",20,true);
-        Zoo myzoo= new Zoo("cccc","a",20);
-        myzoo.displayZoo();
-        System.out.println(myzoo);
-        System.out.println(myzoo.toString());
-        lion2.displayAnimal();
-        System.out.println(lion2);
-        System.out.println(lion2.toString());
+public static void main(String[] args) {
 
-    }
+/*
+Animal lion=new Animal(); //// sans constructeur////
+lion.family="lions";
+lion.name="jjj"
+lion.age=6;
+lion.isMammal=true;
+  Zoo myZoo=new Zoo();
+
+*/
+    // avec constructeur parametré
+    Animal lion=new Animal("lions","aaa",6,true);
+
+    Zoo zzz =new Zoo("mmm","california",20);
+    Zoo z2 =new Zoo("z2","tunis",25);
+    Zoo z3 =new Zoo("z3","eee",25);
+    zzz.displayZoo();
+    lion.displayAnimal();
+
+
+    ///////////////Ajout animal dans zoo
+    Animal giraffe = new Animal("Giraffe","g1",2,true);
+    Animal elephant=new Animal("elephant","elf1",1,true);
+    Animal tigre = new Animal("tigre","t1",5,true);
+    Animal monkey=new Animal("monkey","s1",1,true);
+    System.out.println("Ajout de la Girafe : " + zzz.addAnimal(giraffe));
+    System.out.println("Ajout de la lion : " + zzz.addAnimal(lion));
+    System.out.println("Ajout de l'elephant : " + zzz.addAnimal(elephant));
+    System.out.println("Ajout de singe : " + zzz.addAnimal(monkey));
+    System.out.println("Ajout de tigre : " + zzz.addAnimal(tigre));
+
+    //verifier si on peut ajouter animal existant****
+    System.out.println("Ajout de la Girafe : " + zzz.addAnimal(giraffe));
+
+    System.out.println("display animals dans un zoo");
+    zzz.displayAnimals();
+
+    System.out.println("recherche animal dans un zoo");
+    System.out.println(zzz.searchAnimal(lion));
+
+    System.out.println("supprimer animal dans un zoo");
+    System.out.println(zzz.removeAnimal(monkey));
+
+    System.out.println("verifier si le zoo est complet");
+    System.out.println(zzz.isZooFull());
+
+    System.out.println("comparer 2 zoo");
+
+    System.out.println(z3.comparerZoo(zzz,z2));
+
+    ////////////////
+    Aquatic a=new Aquatic("aaa","dophin",6,true,"mer");
+    Terrestrial t=new Terrestrial("aaa","dophin",6,true,5);
+    Dophin dophin=new Dophin(14,"aaa","dophin",6,true,"mer");
+    Peinguin peinguin=new Peinguin(16,"aaa","dophin",6,true,"mer");
+
+
+
+
+
+}
 }
--- Zoo.java
+++ Zoo.java
@@ -1,17 +1,127 @@
+import javax.lang.model.type.NullType;
+
 public class Zoo {
-    Animal[] animals;
-    String name ;
-    String city;
-    int nbrCages;
-    public Zoo(){}
-    public Zoo(String name,String city,int nbrCages){
-        this.name=name;
-        this.city=city;
-        this.nbrCages=nbrCages;
+   private String name;
+     private String city;
+    static final int nbrCages=15;
+    private Animal[] animals;
+    public Zoo (String name,String city,int nbrCages)
+    {
+        animals = new Animal[nbrCages];
+        this.name = name;
+        this.city = city;
+       // this.nbrCages = nbrCages;
     }
-    public void displayZoo() {
-      System.out.println(this.name);
-      System.out.println(this.city);
-      System.out.println(this.nbrCages);
+    public String getName() {
+        return name;
     }
+    public String getCity() {
+        return city;
+    }
+    public int getNbrCages() {
+        return nbrCages;
+    }
+    public Animal[] getAnimals() {
+        return animals;
+    }
+    public void setAnimals(Animal[] animals) {
+        this.animals = animals;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void displayZoo()
+    {
+        System.out.println("affichage des données de zoo : ");
+        System.out.println("zoo name :" +this.name);
+        System.out.println("zoo city :" +this.city);
+        System.out.println("nombre de cages :" + this.nbrCages);
+    }
+   public int searchAnimal(Animal animal)
+    {
+        for (int i=0;i<animals.length;i++){
+            if (animals[i]!=null && animals[i].get_Name()==animal.get_Name()){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+   public boolean addAnimal(Animal a)
+    {
+
+
+            int recherche = searchAnimal(a);
+            if (recherche == -1) {
+                for (int i = 0; i < animals.length; i++) {
+                    if (animals[i] == null) {      //if (!isZoofull())
+                        animals[i] = a;
+                        return true;
+                    }
+
+                }
+            } else
+                System.out.println("animal existant");
+            return false;
+
+
+    }
+    public void displayAnimals()
+    {
+        int nb=0;
+        for (int i=0;i<animals.length;i++){
+            if (animals[i]!=null) {
+                nb++;
+                System.out.println("aniaml"+ i);
+                System.out.println(animals[i].get_Name());
+                System.out.println(animals[i].get_Age());
+                System.out.println(animals[i].get_family());
+                System.out.println(animals[i].get_isMammal());
+            }
+        }
+        System.out.println(nb);
+    }
+
+    public boolean removeAnimal(Animal animal)
+    {
+            int recherche = searchAnimal(animal);
+        if (recherche==-1)
+        {
+            System.out.println("animal n'existe pas");
+            return false;
+        }
+        else {
+
+           animals[recherche]=null;
+            System.out.println("l'animal suprrimé est :" + animal.get_Name());
+           return true;
+        }
+    }
+    public boolean isZooFull() {
+
+            if (animals.length==nbrCages) {
+                System.out.println("zoo full");
+                return true;
+            }
+                return false;
+        }
+        public Zoo comparerZoo(Zoo z1, Zoo z2)
+        {
+            if (z1.getAnimals().length>z2.getAnimals().length) {
+                return z1;
+            }
+            return z2;
+        }
+
+
+
+
+
+
+
 }
